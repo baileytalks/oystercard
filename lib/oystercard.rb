adfbb1d62f4252d5aba7cda48db1b7a00721315a
Bailey Kursar <bkursar@gmail.com>
2017-04-17 21:07:23 +0100

    Finished step 8, Add touch in/out support (apart from used in_journey, not in_journey?)

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,19 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   MAXIMUM_BALANCE = 90
 
   def initialize(balance=0)
     @balance = balance
+    @in_journey = false
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
   def top_up(n)
@@ -16,7 +25,7 @@ class Oystercard
   end
 
   def deduct(n)
-    @balance -= n   
+    @balance -= n
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -30,4 +30,24 @@ describe Oystercard do
     end
   end
 
+  describe '#in_journey?' do
+    it "expects a newly created Oystercard to not be in use on a journey" do
+      expect(subject.in_journey).to eq false
+    end
+  end
+
+  describe '#touch_in' do
+    it "expects a card that has been touched in to be in use on a journey" do
+      subject.touch_in
+      expect(subject.in_journey).to eq true
+    end
+  end
+
+  describe '#touch_out' do
+    it "expects a card that has been touched out to not be in use on a journey" do
+      subject.touch_out
+      expect(subject.in_journey).to eq false
+    end
+  end
+
 end
